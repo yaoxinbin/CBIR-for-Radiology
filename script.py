03a1fe442f88c47d3e6902b0f76341a510c0f833
ysriram1 <ysriram@umich.edu>
2017-05-09 19:01:05 -0500

    added JS with bag of words approach

--- script.py
+++ script.py
@@ -8,6 +8,9 @@ import numpy as np
 from numpy.matlib import repmat# for repmat
 
 from scipy.spatial.distance import * # dist and sim metrics
+from sklearn.cluster import KMeans 
+
+seed = 99 # for randomized computations
 
 start = True # set to True to run the functions
 
@@ -162,8 +165,32 @@ def add_image_features(image_dict, kind = 'orb'):
 	return image_feats_dict
 
 
+# returns a vector of distance to each of the training arrays from query array
+# using jensen shannon divergence
+def jensen_shannon_div(query_arr,train_mat):
+
+	query_mat = repmat(query_arr, len(train_mat), 1)
+	
+	mat_sum = 0.5*(query_mat + train_mat)
+
+	D1 = query_mat * np.log2(np.divide(query_mat, mat_sum))
+
+	D2 = train_mat * np.log2(np.divide(train_mat, mat_sum))
+
+	# convert all nans to 0
+
+	D1[np.isnan(D1)] = 0
+	
+	D2[np.isnan(D2)] = 0
+	
+	JS_mat = 0.5 * (np.sum(D1,1) + np.sum(D2,1)) 
+
+	return JS_mat 
+
+
 # returns keys of the top-k images
 # euclidean and cosine used for histogram and sift has its own method using bag of words approach
+# k used if the bag_of_words approach is used
 
 def calc_dist_sim(query_image_arr, image_feats_dict, method='orb', k=10):
 
@@ -216,7 +243,7 @@ def calc_dist_sim(query_image_arr, image_feats_dict, method='orb', k=10):
 		matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
 		# extract orb features from query image
-		query_feats = image_descriptors.sift(query_image_arr)
+		query_feats = image_descriptors.orb(query_image_arr)
 
 		# find the match between query and each training image
 		for image in image_feats_dict:
@@ -232,18 +259,48 @@ def calc_dist_sim(query_image_arr, image_feats_dict, method='orb', k=10):
 	# bag of visual words approach to retreive images
 	if method == 'bag_of_words':
 
-		# apply k-means to find the centroids 
+		# apply k-means to find the centroids
+		train_feats = image_feats_dict.values()
 
-		# find closest center to each feature 
+		k_means = KMeans(n_clusters=k, random_state=seed).fit(train_feats)
 
-		# create DTM matrix (term - center, d - image) - fills in with term freq not tfidf
+		cluster_clusters = k_means.cluster_centers_
 
-		# extract query feats and create term freq vector similar to above
+		# TODO: loops are too slow --> replace with nunmpy matrix math
+		# find closest center to each image keypoint and generate histogram
+		image_hist_dict = {}
+
+		for image_id, each_image in image_feats_dict.values():
+
+			image_hist_dict[image_id] = [0] * k
+
+			for key_point in each_image:
+
+				diff = cluster_centers_ - repmat(keypoint,len(cluster_centers_), 1)
+
+				euclidean_dists = np.apply_along_axis(np.linalg.norm, 1, diff)
+
+				image_hist_dict[image_id][np.argmin(euclidean_dists)] += 1 # add to frequency of correponding center
+
+		# extract orb features from query image
+		query_feats = image_descriptors.orb(query_image_arr)
+
+		query_hist = np.array([0] * k)
+
+		# convert query_feats into the histogram like above
+		for key_point in query_feats:
+
+			diff = cluster_centers_ - repmat(keypoint,len(cluster_centers_), 1)
+
+			euclidean_dists = np.apply_along_axis(np.linalg.norm, 1, diff)
+
+			query_hist[np.argmin(euclidean_dists)] += 1
+
 
 		# use shannon divergence to find distance to each image from query
+		JS_distances = jensen_shannon_div(query_hist, np.array(image_hist_dict.values()))
 
-		pass
-
+		image_sim_dist_dict = dict((key, val) for key,val in zip(image_feats_dict.keys(),JS_distances))
 
 	return image_sim_dist_dict
 
