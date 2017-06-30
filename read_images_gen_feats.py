9ccc54a748f1113e52a4587792a3a96d8b521ea9
Yarlagadda <SYARLAG1@depaul.edu>
2017-06-29 23:22:25 -0500

    made changes to accomdate gray scale images+other images

--- read_images_gen_feats.py
+++ read_images_gen_feats.py
@@ -13,7 +13,7 @@ def read_dicom_image(image_path):
 
 # returns a dictionary of images extracted from folder
 def read_images_from_folder(location):
-	
+
 	image_dict = {}
 	image_path_list = os.listdir(location)
 
@@ -24,7 +24,7 @@ def read_images_from_folder(location):
 			image_dict[image_path]=read_dicom_image(location+image_path).pixel_array
 
 		else:
-		
+
 			image_dict[image_path]=cv2.imread(location+image_path)
 
 	return image_dict
@@ -44,26 +44,47 @@ class image_descriptors():
 		orb = cv2.ORB()
 
 		# detector of the points in the image
-		#detector = cv2.FeatureDetector_create("SIFT") 
+		#detector = cv2.FeatureDetector_create("SIFT")
 
-		# extractor of the detected points 
+		# extractor of the detected points
 		#descriptor = cv2.DescriptorExtractor_create("SIFT")
- 
+
 		#skp = detector.detect(pixel_array)
 
 		_, feats = orb.detectAndCompute(gray,None)
 
 		return feats
 
-	# TODO: if ellipse=True, then only ellipse of image ketypoints extracted 
+	# if ellipse=True, then only ellipse of image keypoints extracted
 	@staticmethod
 	def sift(pixel_array, ellipse=False):
 
-		# convert to grayscale
-		gray= cv2.cvtColor(pixel_array,cv2.COLOR_BGR2GRAY)
+		print(True)
+
+		# convert to grayscale only if color - i.e. more than 1 channels
+		# image will have a color channel dim -> hence 3
+		if len(pixel_array.shape) == 3:
+
+			print(str(3))
+			# TODO: replace with something better
+			# hacky way of dealing with images that have color channels first
+			if pixel_array.shape[0] == 3: pixel_array = pixel_array.T
+
+			try:
+				gray= cv2.cvtColor(pixel_array,cv2.COLOR_BGR2GRAY)
+
+			except: # some pixel arrays have more than 3 color channels, we skip them
+
+				return None
+
+		elif len(pixel_array.shape) == 2:
+
+			print(str(2))
+
+			gray = pixel_array
 
 		if ellipse:
-			
+
 			# get image dimensions
 			x,y = gray.shape[:2]
 
@@ -91,57 +112,57 @@ class image_descriptors():
 	@staticmethod
 	def hist(pixel_array, bins = 10):
      # with thanks to http://www.pyimagesearch.com/2014/12/01/complete-guide-building-image-search-engine-python-opencv/
-     # and https://github.com/danuzclaudes/image-retrieval-OpenCV/		
+     # and https://github.com/danuzclaudes/image-retrieval-OpenCV/
 
           hsv = cv2.cvtColor(pixel_array, cv2.COLOR_BGR2HSV)
 
           feats = []
-          
+
           # get image dimensions
           x,y = hsv.shape[:2]
 
           dx, dy = int(x*0.5), int(y*0.5) # get halfway point to divde into segements
-          
+
           # create 4 segements (top-left, bottom-left, top-right, bottom-right)
           regions = [(0,dx,0,dy), (0,dx,dy,y), (dx,x,0,dy), (dx,x,dy,y)]
-          
+
           # elliptical mask for the center of the image
           # major and minor axis
           ex, ey = int(x*0.75)/2, int(y*0.75)/2
-          
+
           ellipse_mask = np.zeros([x,y], dtype = "uint8")
 
-          # make mask into an ellipse          
+          # make mask into an ellipse
           cv2.ellipse(ellipse_mask, (dx, dy), (ex, ey), 0, 0, 360, 255, -1)
-          
+
           # gen histogram
           hist = cv2.calcHist([hsv],[0,1,2],ellipse_mask,[bins],[0,180,0,256,0,256],True)
-          
+
           # normalize
           hist = cv2.normalize(hist).flatten()
-          
+
           feats.extend(hist)
-          
-          
+
+
           # loop through the segements and extract the histograms
           for area in regions:
 
               # a second mask is needed for each of the corners (everything other than the ellipse)
               corner_mask = np.zeros([x,y], dtype = "uint8")
-              
+
               # draw rectangle mask on corner_mask object
-              corner_mask[area[0]:area[1], area[2]:area[3]] = 255        
+              corner_mask[area[0]:area[1], area[2]:area[3]] = 255
 
               corner_mask = cv2.subtract(corner_mask, ellipse_mask)
-              
+
               # gen histogram like before
               hist = cv2.calcHist([hsv],[0,1,2],corner_mask,bins,[0,180,0,256,0,256])
 
               hist = cv2.normalize(hist).flatten()
-              
+
               feats.extend(hist)
-           
-           
+
+
           return feats
 
 	# TODO
@@ -157,7 +178,7 @@ class image_descriptors():
 
 
 # for each image in dict, extract image features and add to new dict
-# this function will updated as more feature extraction techniques 
+# this function will updated as more feature extraction techniques
 # are introduced
 def add_image_features(image_dict, kind = 'sift', ellipse=False):
 
